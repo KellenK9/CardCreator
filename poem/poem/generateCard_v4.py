26f76f5f614a74423f23982d8d5e82306d4789ec
KellenK9 <55595944+KellenK9@users.noreply.github.com>
2024-11-01 17:06:45 -0500

    Comment

--- poem/poem/generateCard_v4.py
+++ poem/poem/generateCard_v4.py
@@ -287,3 +287,11 @@ for card in loaded_json["cards"]:
 
 # generate_card(temp_dict).show()
 # generate_equipment_card(temp_dict_equipment).show()
+
+# 19 total spells
+# 14 total champions
+# 7 air equipment
+# 6 earth equipment
+# 5 fire equipment
+# 6 water equipment
+# 57 total cards
