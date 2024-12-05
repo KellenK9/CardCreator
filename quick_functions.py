e034bca3a627abb88ba55119889d0c6a87dc8e35
KellenK9 <55595944+KellenK9@users.noreply.github.com>
2024-12-04 18:50:58 -0600

    Finished logic

--- quick_functions.py
+++ quick_functions.py
@@ -9,21 +9,24 @@ class QuickFunctions:
             rows = list(reader)
 
             sum_arr = [0, 0, 0, 0]
-
-            # Skip the header if it exists
             for row in rows:
                 if row[0] != "Card Name" and row[0] != "Deck Totals":
                     sum_arr[0] += float(row[2])
+                    sum_arr[1] += float(row[3])
+                    sum_arr[2] += float(row[4])
+                    sum_arr[3] += float(row[5])
 
         # Append the sum as a new row and overwrite the file
         with open(csv_path, mode="w", newline="") as file:
             writer = csv.writer(file)
 
-            # Write the original data
-            writer.writerows(rows)
+            # Write the original data excluding the final row
+            writer.writerows(rows[:-1])
 
             # Append the sum as a new row
-            writer.writerow(["", "", f"Sum: {column_sum}"])
+            writer.writerow(
+                ["Deck Totals", "", sum_arr[0], sum_arr[1], sum_arr[2], sum_arr[3]]
+            )
 
 
 QuickFunctions.sum_csv("starter_decks.csv")
